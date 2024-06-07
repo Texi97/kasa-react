753a3d693a03df3616323215e0b934fe6692d725
Texi97 <teri.trouss@gmail.com>
2024-06-07 10:39:33 +0200

    Changes risk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+<<<<<<< Updated upstream
 import About from "./pages/About";
 import ErrorPage from "./pages/ErrorPage";
 import Lodging from "./pages/Lodging";
+=======
+
+import Error from "./pages/Error";
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/logement/:id" element={<Lodging />} />
+
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au-dessus */}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,15 +1,29 @@
 import React from "react";
-import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+<<<<<<< Updated upstream:src/pages/ErrorPage.js
 import "../styles/_error404.scss";
 import Error from "../components/Error";
+=======
+
+import "../styles/index.css";
+import { DisappearingFeatures } from "../components/DisappearingFeatures";
+import ShuffleHero from "../components/ShuffleHero";
+import { CountUpStats } from "../components/CountUpStats";
+>>>>>>> Stashed changes:src/pages/Error.js
 
 const ErrorPage = () => {
   return (
     <div>
       <div className="container_page">
+<<<<<<< Updated upstream:src/pages/ErrorPage.js
         <Navigation />
         <Error />
+=======
+        <ShuffleHero />
+
+        <DisappearingFeatures />
+        <CountUpStats />
+>>>>>>> Stashed changes:src/pages/Error.js
       </div>
       <div className="container_footer">
         <Footer />
